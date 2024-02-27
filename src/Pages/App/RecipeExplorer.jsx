0d6bf94aa2dcc91bed384241f67ea0dfdc34076d
tecnycompu <tecnycompu@gmail.com>
2024-02-27 08:17:02 -0500

    UPDATE: ingreso de busqueda...

--- src/Pages/App/RecipeExplorer.jsx
+++ src/Pages/App/RecipeExplorer.jsx
@@ -6,36 +6,40 @@ import "slick-carousel/slick/slick-theme.css";
 const RecipeExplorer = () => {
   const [showModal, setShowModal] = useState(false);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const recipes = [
     {
       title: 'Paella Mixta (España)',
       image: 'https://estaticos-cdn.prensaiberica.es/clip/9cb62572-b4fe-4969-b0fb-0ee876339b11_16-9-aspect-ratio_default_0.jpg',
-      description: 'Compite con la tortilla de patatas por ser la comida más famosa de nuestro país. Es un plato típicamente mediterráneo, muy nutritivo y cardiosaludable que tiene su origen en la Albufera de Valencia. Admite todo tipo de ingredientes desde carnes a verduras, el único indispensable es el arroz. Así se prepara: Se pueden suprimir los que no gusten o no convengan. Se pone el pollo cubierto de agua fría en una cazuela -no conviene excederse con la cantidad de agua para que no sobre al agregarla al arroz- con un trozo de cebolla, un diente de ajo y una rama de perejil. Una vez cocido, se saca y se deja enfriar, reservando el caldo. Las almejas, lavadas, se ponen en un cazo con un pocillo de agua sobre el fuego, y a medida que abren se pasan con una espumadera a otro recipiente, quitándoles media cáscara. El caldo se filtra con un paño fino y se mezcla con el otro caldo. En otra cazuela, con la mitad del aceite que se empleará para hacer la paella, se fríe el lomo cortado en trozos. Ya frito, se agrega una cucharada de cebolla picada y el tomate, se rehoga todo y se cuece hasta que esté tierno. A continuación se añaden las salchichas, el congrio cortado, los calamares limpios y troceados -sin la tinta-, los langostinos, las almejas y el pollo, cortado en trozos y sin hueso. Ingredientes: 1/2 kg de arroz, 250 g de salchichas blancas, 6 langostinos, 1/4 kg de congrio, 1/4 kg de calamares pequeños, 1/4 kg de almejas, 200 g de lomo de cerdo, 1 pollo pequeño, 1 cucharada de tomate, 1 lata pequeña de pimientos, 1 lata de guisantes, cebolla, ajo, perejil, azafrán, aceite y sal.'
+      description: 'Compite con la tortilla de patatas por ser la comida más famosa de nuestro país. Es un plato típicamente mediterráneo, muy nutritivo y cardiosaludable que tiene su origen en la Albufera de Valencia. Admite todo tipo de ingredientes desde carnes a verduras, el único indispensable es el arroz.'
     },
     {
       title: 'American Burger (EEUU)',
       image: 'https://estaticos-cdn.prensaiberica.es/clip/b05a8713-4d7b-4e7b-a7d8-9a82aa0bfdf0_16-9-aspect-ratio_default_0.jpg',
-      description: 'Así se prepara: Se pueden suprimir los que no gusten o no convengan. Se pone el pollo cubierto de agua fría en una cazuela -no conviene excederse con la cantidad de agua para que no sobre al agregarla al arroz- con un trozo de cebolla, un diente de ajo y una rama de perejil. Una vez cocido, se saca y se deja enfriar, reservando el caldo. Las almejas, lavadas, se ponen en un cazo con un pocillo de agua sobre el fuego, y a medida que abren se pasan con una espumadera a otro recipiente, quitándoles media cáscara. El caldo se filtra con un paño fino y se mezcla con el otro caldo. En otra cazuela, con la mitad del aceite que se empleará para hacer la paella, se fríe el lomo cortado en trozos. Ya frito, se agrega una cucharada de cebolla picada y el tomate, se rehoga todo y se cuece hasta que esté tierno. A continuación se añaden las salchichas, el congrio cortado, los calamares limpios y troceados -sin la tinta-, los langostinos, las almejas y el pollo, cortado en trozos y sin hueso.'
+      description: 'El icono de la gastronomía norteamericana debe su nombre a la ciudad germana de Hamburgo, aunque su origen se remonta al Imperio Romano. Relacionada siempre con la comida basura, lo cierto es que es un plato muy recomendable siempre que se use carne de calidad, ingredientes frescos y no se abuse de las salsas.'
     },
     {
       title: 'Sushi (Japón)',
       image: 'https://estaticos-cdn.prensaiberica.es/clip/2e86c5b0-4756-4eea-94d6-895ab777ed87_16-9-aspect-ratio_default_0.jpg',
-      description: 'Así se prepara: Primero hay que quitar el almidón al arroz. Para ello lo ponemos en un colador y lo mantenemos bajo el agua del grifo (cuando el almidón haya desaparecido el agua filtrada por el arroz dejará de ser blanquecina y volverá a ser transparente). Ponemos a hervir el arroz durante unos 20 minutos a fuego lento. Cuando esté hecho lo pasamos a una fuente y lo dejamos enfriar. Mientras tanto, preparamos la mezcla para el arroz. En un recipiente ponemos tres cucharadas de vinagre y una de azúcar, y agitamos hasta que este quede casi disuelto. Añadimos esta mezcla al arroz. Ingredientes: Una taza de arroz, vinagre de arroz, azúcar, algas nori, wasabi, salsa de soja, cualquier pescado fresco, pepino o aguacate. Es necesaria una esterilla o makisu para hacer los rollos.'
+      description: 'El sushi, al contrario de lo que muchos piensan, no es solo pescado crudo. El plato que Japón ha logrado exportar al resto del mundo se prepara a base de arroz cocido, algas, verduras y pescado crudo. Es rico en Omega 3, por lo que ayuda a prevenir enfermedades del corazón, problemas cerebrovasculares y artritis. '
     }
   ];
 
+  const filteredRecipes = recipes.filter(recipe =>
+    !searchTerm || recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   const sliderSettings = {
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1
   };
 
   const openModal = (index) => {
-    setSelectedRecipe(recipes[index]);
+    setSelectedRecipe(filteredRecipes[index]);
     setShowModal(true);
   };
 
@@ -45,12 +49,20 @@ const RecipeExplorer = () => {
 
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Buscar receta..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="border border-gray-300 rounded-md p-2 mb-4"
+      />
+
       <Slider {...sliderSettings}>
-        {recipes.map((recipe, index) => (
-          <div key={index} className="bg-white p-4 rounded-lg">
+        {filteredRecipes.map((recipe, index) => (
+          <div key={index} className="bg-white p-4 rounded-lg ">
             <img src={recipe.image} alt={recipe.title} className="mx-auto mb-4" />
-            <h2 className="text-xl font-bold mb-2">{recipe.title}</h2>
-            <p className="text-gray-700">{recipe.description.substring(0, 100)}</p>
+            <h2 className="text-3xl font-bold mb-2 ">{recipe.title}</h2>
+            <p className="text-gray-700 text-2xl">{recipe.description.substring(0, 100)}</p>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={() => openModal(index)}>Ver Receta</button>
           </div>
         ))}
@@ -58,10 +70,10 @@ const RecipeExplorer = () => {
 
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-8 rounded-lg max-w-xl">
-            <h2 className="text-xl font-bold mb-4">{selectedRecipe.title}</h2>
+          <div className="bg-white p-8 rounded-lg max-w-screen-lg">
+            <h2 className="text-3xl font-bold mb-4">{selectedRecipe.title}</h2>
             <img src={selectedRecipe.image} alt={selectedRecipe.title} className="mx-auto mb-4" />
-            <p className="text-gray-700 mb-4">{selectedRecipe.description}</p>
+            <p className="text-gray-700 mb-4 text-2xl">{selectedRecipe.description}</p>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={closeModal}>Cerrar</button>
           </div>
         </div>
